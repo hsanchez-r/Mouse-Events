3f4709a3f33120cd2a075db58935b74bc5f17b9e
iamhector <hectors.r@icloud.com>
2019-09-01 14:06:58 -0400

    Continued working on mouse events. Also, implemented the MouseMotionListener interface to see other actions that I can do with the mouse

--- Eventos_Raton.java
+++ Eventos_Raton.java
@@ -25,18 +25,57 @@ class MarcoRaton extends JFrame{
 		EventosDeRaton miraton = new EventosDeRaton();
 		
 		addMouseListener(miraton);
+		addMouseMotionListener(miraton);
 		
 	}
 		
 }
 
-class EventosDeRaton extends MouseAdapter{
+class EventosDeRaton extends MouseAdapter implements MouseMotionListener{
 
-	public void mouseClicked(MouseEvent e) {
+	/*public void mouseClicked(MouseEvent e) {
 		
-		System.out.println("Acabas de hacer click");
+		System.out.println("Coordenada X: " + e.getX() + " Coordenada Y: " + e.getY());
+		
+		System.out.println("Este es el numero de veces que has hecho click: " + e.getClickCount());
+		
+		
+	}*/
+	
+	public void mousePressed(MouseEvent e) {
+		
+		//to know which mouse button have been pressed by the user
+		
+		int boton1 = MouseEvent.BUTTON1_DOWN_MASK; //remember that MouseEvent class extends from InputEvent class (check Java API for further reference)
+		int boton2 = MouseEvent.BUTTON3_DOWN_MASK;
+		
+		if(e.getModifiersEx() == boton1) {
+			
+			System.out.println("Usted ha presionado el boton izquierdo del mouse");
+			
+		} else if(e.getModifiersEx() == boton2) {
+			
+			System.out.println("Usted ha presionado el  boton derecho del mouse");
+			
+		}
+		
+		//didn't use public static final int BUTTON2_DOWN_MASK because I'm working from my laptop
+		
+		
+	}
+	
+	public void mouseDragged(MouseEvent e) {
+		
+		System.out.println("Estas arrastrando");
+		
+	}
+	
+	public void mouseMoved(MouseEvent e) {
+		
+		System.out.println("Estas moviendo");
 		
 	}
 	
 	
 }
+
